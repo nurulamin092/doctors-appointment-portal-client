992db67e97c231119647311a49e7c49880bc9948
nurulamin092 <mnurulamin092@gmial.com>
2022-05-18 15:17:55 +0600

    admin button added

--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const UserRow = ({ user }) => {
-    const { email } = user
+const UserRow = ({ user, refetch }) => {
+    const { email, role } = user;
+    const makeAdmin = () => {
+        fetch(`http://localhost:5000/user/admin/${email}`, {
+            method: 'PUT',
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                refetch();
+                toast.success(`successfully made and admin`)
+
+            })
+    }
     return (
 
         <tr>
             <th>1</th>
             <td>{email}</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
+            <td>{role !== 'admin' && <button onClick={makeAdmin} className="btn btn-xs">Admin</button>}</td>
+            <td><button className="btn btn-xs">Remove User</button></td>
         </tr>
 
     );
--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -3,15 +3,20 @@ import { useQuery } from 'react-query';
 import Loading from '../Shared/Loading';
 import UserRow from './UserRow';
 const Users = () => {
-    const { isLoading, data: users } = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()))
+    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+        method: 'GET',
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res => res.json()));
     if (isLoading) {
         return <Loading></Loading>
     }
     return (
         <div>
             <h2 className="text-2xl text-blue-300">All User:{users.length}</h2>
-            <div class="overflow-x-auto">
-                <table class="table w-full">
+            <div className="overflow-x-auto">
+                <table className="table w-full">
                     <thead>
                         <tr>
                             <th></th>
@@ -23,7 +28,7 @@ const Users = () => {
                     <tbody>
 
                         {
-                            users.map(user => <UserRow key={user._id} user={user}></UserRow>)
+                            users.map(user => <UserRow key={user._id} user={user} refetch={refetch}></UserRow>)
                         }
 
                     </tbody>
