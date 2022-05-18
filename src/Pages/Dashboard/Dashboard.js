b833502f6bfefdd78d51fe61dbb931127fd15b85
nurulamin092 <mnurulamin092@gmial.com>
2022-05-18 16:26:03 +0600

    procteted admin router

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-
+import useAdmin from '../../hooks/useAdmin';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 const Dashboard = () => {
+    const [user] = useAuthState(auth)
+    const [admin] = useAdmin(user);
     return (
         <div className="drawer drawer-mobile">
             <input id="dashboard-sidebar" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col">
                 <h2 className='text-3xl font-bold text-purple-500'>Welcome To Your Dashboard</h2>
                 <Outlet>
-
                 </Outlet>
 
             </div>
@@ -19,7 +22,7 @@ const Dashboard = () => {
                     <li><Link to="/dashboard">My Appointment</Link></li>
                     <li><Link to='/dashboard/review'>My Review</Link></li>
                     <li><Link to='/dashboard/history'>My History</Link></li>
-                    <li><Link to='/dashboard/users'>All Users</Link></li>
+                    <li>{admin && <Link to='/dashboard/users'>All Users</Link>}</li>
                 </ul>
 
             </div>
