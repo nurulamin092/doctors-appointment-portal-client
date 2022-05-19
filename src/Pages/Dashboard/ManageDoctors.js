28546a8cea3fd7ffda7010c99b45792396027f78
nurulamin092 <mnurulamin092@gmial.com>
2022-05-19 21:20:40 +0600

    Mange Doctors and delete doctor with authorization

--- src/Pages/Dashboard/ManageDoctors.js
+++ src/Pages/Dashboard/ManageDoctors.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading';
+import DoctorRow from './DoctorRow';
 
 const ManageDoctors = () => {
+    const { data: doctors, isLoading, refetch } = useQuery('doctors', () =>
+        fetch('http://localhost:5000/doctor', {
+            headers: {
+                authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            }
+        }).then(res => res.json()));
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-            <h3 className='text-3xl'>Manage Doctor</h3>
+            <h3 className='text-3xl'>Manage Doctor:{doctors.length}</h3>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Specialty</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            doctors.map((doctor, index) => <DoctorRow
+                                key={doctor._id}
+                                doctor={doctor}
+                                index={index}
+                                refetch={refetch}
+                            ></DoctorRow>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
