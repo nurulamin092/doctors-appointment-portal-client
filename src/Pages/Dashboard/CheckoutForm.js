eb9372b9f956946286c8792c8571ccb94f0777c1
nurulamin092 <mnurulamin092@gmial.com>
2022-05-22 13:10:50 +0600

    Store payment on database and update appointment data as well

--- src/Pages/Dashboard/CheckoutForm.js
+++ src/Pages/Dashboard/CheckoutForm.js
@@ -8,9 +8,10 @@ const CheckoutForm = ({ appointment }) => {
     const elements = useElements();
     const [cardError, setCardError] = useState('');
     const [success, setSuccess] = useState('');
+    const [processing, setProcessing] = useState(false);
     const [transactionId, setTransactionId] = useState('');
     const [clientSecret, setClientSecret] = useState('');
-    const { price, patientName, patient } = appointment;
+    const { _id, price, patientName, patient } = appointment;
     useEffect(() => {
         fetch('http://localhost:5000/create-payment-intent', {
             method: 'POST',
@@ -44,9 +45,10 @@ const CheckoutForm = ({ appointment }) => {
         });
 
         setCardError(error?.message || '');
+        setSuccess('');
+        setProcessing(true);
 
         //confirm card payment
-        setSuccess('');
         const { paymentIntent, error: intentError } = await stripe.confirmCardPayment(
             clientSecret,
             {
@@ -61,13 +63,34 @@ const CheckoutForm = ({ appointment }) => {
         )
         if (intentError) {
             setCardError(intentError?.message);
+            setProcessing(false);
 
         }
         else {
             setCardError('');
             setTransactionId(paymentIntent.id);
             console.log(paymentIntent);
-            setSuccess('Congrats! Your payment is completed.')
+            setSuccess('Congrats! Your payment is completed.');
+
+            //store payment on database
+
+            const payment = {
+                appointment: _id,
+                transactionId: paymentIntent.id,
+            }
+            fetch(`http://localhost:5000/booking/${_id}`, {
+                method: "PATCH",
+                headers: {
+                    'content-type': 'application/json',
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                },
+                body: JSON.stringify(payment)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    setProcessing(false)
+                    console.log(data);
+                })
         }
     }
     return (
